3943494148c80318744b11742b5f223424114f76
Raihan Nismara <raihan@d3labs.io>
2026-06-18 07:50:03 +0700

    chore: fix error deploy vercel

--- crawler/utils.ts
+++ crawler/utils.ts
@@ -1,7 +1,7 @@
 export const range = (start: number, end: number): number[] =>
   start === end ? [start] : [start, ...range(start + 1, end)]
 
-export const sleep = async ({ delay = 2000, throwReject = false }) => new Promise((resolve, reject) => {
+export const sleep = async ({ delay = 2000, throwReject = false }) => new Promise<void>((resolve, reject) => {
   setTimeout(() => {
     if (throwReject) reject({ timeout: true })
     else resolve()
